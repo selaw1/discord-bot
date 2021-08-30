4c094aa3885f7612c0fd32533acb509fde10b76f
selaw1 <88832989+selaw1@users.noreply.github.com>
2021-08-30 03:42:28 +0300

    commit

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1cfufw*gr1$#6+qn(r^e7nw1y5bm&lq@vsszuo3sf=o!+h#r@-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
